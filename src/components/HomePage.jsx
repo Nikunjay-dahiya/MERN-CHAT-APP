e990a70a817525b1bb82a276d272d04952212e50
Nikunjay-dahiya <chaudharynikunjay@gmail.com>
2024-08-31 08:29:37 +0530

    first commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Sidebar from './Sidebar'
 import MessageContainer from './MessageContainer'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 const HomePage = () => {
-    return(
-        <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur bg-opacity-0'>
-<Sidebar/>
-<MessageContainer/>
-        </div>
-    )
+  const { authUser } = useSelector(store => store.user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!authUser) {
+      navigate("/login");
+    }
+  }, []);
+  return (
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <Sidebar />
+      <MessageContainer />
+    </div>
+  )
 }
 
 export default HomePage
